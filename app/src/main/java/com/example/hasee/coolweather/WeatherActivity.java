95f4de688233a64014d1b1af8929826e76a9969a
Leung <394869042@qq.com>
2017-04-23 19:42:16 +0800

    修改程序图标和名称

--- app/src/main/java/com/example/hasee/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/hasee/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.hasee.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.hasee.coolweather.gson.Forecast;
 import com.example.hasee.coolweather.gson.Weather;
+import com.example.hasee.coolweather.service.AutoUpdateService;
 import com.example.hasee.coolweather.util.HttpUtil;
 import com.example.hasee.coolweather.util.Utility;
 
@@ -211,6 +213,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     /**
